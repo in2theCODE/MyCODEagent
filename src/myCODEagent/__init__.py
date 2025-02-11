f5fb79e35026bcf08bfe4702ba64b599ef44b4c0
in2thecode <justin@in2thecode.com>
2025-02-11 15:53:40 -0500

    updated directory

---
+++ src/myCODEagent/__init__.py
@@ -0,0 +1,6 @@
+"""PAIC Python Starter package."""
+
+from .db import models
+from .main import *
+
+__version__ = "0.1.1"
--- src/myCODEagent/database.py
+++ src/myCODEagent/database.py
@@ -2,8 +2,6 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
-from .models import Base
-
 DATABASE_URL = "postgresql+asyncpg://user:password@localhost/aiden"
 
 engine = create_async_engine(DATABASE_URL, poolclass=NullPool, echo=True)
